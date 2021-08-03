5e402bb5c04fb62ce87aea991458e30d4255d05e
Pavel <86340235+roothere@users.noreply.github.com>
2021-08-04 02:29:19 +0300

    bugFix

--- Assets/Scripts/CloudCrafter.cs
+++ Assets/Scripts/CloudCrafter.cs
@@ -6,13 +6,13 @@ public class CloudCrafter : MonoBehaviour
 {
 
     [Header("Set in Inspector")]
-    public int numClouds = 40;
-    public GameObject cloudPrefab;
-    public Vector3 cloudPosMin = new Vector3(-50, -5, 10);
-    public Vector3 cloudPosMax = new Vector3(150, 100, 10);
-    public float cloudScaleMin = 1;
-    public float cloudScaleMax = 3;
-    public float cloudSpeedMult = 0.5f;
+    public int          numClouds = 40;
+    public GameObject   cloudPrefab;
+    public Vector3      cloudPosMin = new Vector3(-50, -5, 10);
+    public Vector3      cloudPosMax = new Vector3(150, 100, 10);
+    public float        cloudScaleMin = 1;
+    public float        cloudScaleMax = 3;
+    public float        cloudSpeedMult = 0.5f;
 
     private GameObject[] cloudInstances;
     // Start is called before the first frame update
