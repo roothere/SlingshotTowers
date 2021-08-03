8cc3f4080e53b9c38c78f4c941db44b27a0cb4b6
Pavel <86340235+roothere@users.noreply.github.com>
2021-08-03 16:47:31 +0300

    last version of book

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -76,6 +76,8 @@ public class Slingshot : MonoBehaviour
             projectileRigidbody.velocity = -mouseDelta * velocityMult;
             FollowCam.POI = projectile;
             projectile = null;
+            MissionDemolition.ShotFired();
+            ProjectileLine.S.poi = projectile;
         }
     }
 }
